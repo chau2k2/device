cec52e6bc02e4e9fa7c430c11abaa2a51cc11d34
châu <minhchau02012002@gmail.com>
2024-02-15 17:36:12 +0700

    ffff

--- Sale/Models/HoaDon.cs
+++ Sale/Models/HoaDon.cs
@@ -1,6 +1,14 @@
-﻿namespace Sale.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sale.Models
 {
     public class HoaDon
     {
+        [Key]
+        public int Id { get; set; }
+        public string SoHoaDon {  get; set; }
+        public DateTime HoaDonDate { get; set; }
+        public double HoaDonTotal { get; set; }
+
     }
 }
--- Sale/Models/HoaDonDetail.cs
+++ Sale/Models/HoaDonDetail.cs
@@ -1,6 +1,15 @@
-﻿namespace Sale.Models
+﻿using Humanizer.Localisation.TimeToClockNotation;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sale.Models
 {
     public class HoaDonDetail
     {
+        [Key]
+        public int Id { get; set; }
+        public int IdHoaDon { get; set; }
+        public int IdLaptop { get; set; }
+        public int Number { get; set; }
+        public double TotalPrice { get; set; }
     }
 }
